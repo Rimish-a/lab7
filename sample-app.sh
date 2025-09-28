92d3d82cee99bc0163a09b52ecbe5475c9a62701
Rimisha <rimishakhadka@studytafensw.edu.au>
2025-09-28 09:52:48 +0000

    initial commit

---
+++ sample-app.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Create temp directories
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+# Copy files into tempdir
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+# Create Dockerfile
+echo "FROM python:3.8-alpine" > tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
+# Build Docker image
+cd tempdir
+docker build -t sampleapp .
+
+# Run container
+docker run -t -d -p 8080:8080 --name samplerunning sampleapp
+
+# Show running containers
+docker ps -a
